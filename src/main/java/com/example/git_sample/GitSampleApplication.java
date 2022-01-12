f95129d04d1f2a00d80f08d3a042d78b20e5c123
JunHyun <moyanada@gmail.com>
2022-01-12 13:39:01 +0900

    first

---
+++ src/main/java/com/example/git_sample/GitSampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.git_sample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitSampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitSampleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/git_sample/GitSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.git_sample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitSampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
